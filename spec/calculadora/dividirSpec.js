1161efe35a81a295706ede6e46fb0f52297dfe0d
Junior6MW <rosinaldocjr@gmail.com>
2019-06-25 15:39:32 -0300

    Implementado função de divisão

--- spec/calculadora/dividirSpec.js
+++ spec/calculadora/dividirSpec.js
@@ -3,7 +3,7 @@ describe("Suite de testes de divisão", function() {
 	var Calculadora = require('../../src/js/calculadora.js');
 
 	it ('deve retorna 2 para 6 e 3', function() {
-		expect(Calculadora.dividir(6, 2)).toEqual(2);
+		expect(Calculadora.dividir(6, 3)).toEqual(2);
 	});
 
 	it ('deve retornar 2 para 6 e 3 no formato texto', function() {
@@ -11,7 +11,7 @@ describe("Suite de testes de divisão", function() {
 	});
 
 	it('deve retornar 1.5 para 4.5 e 3', function() {
-		expect(Calculadora.dividir(4.5, 3).toEqual(1.5);
+		expect(Calculadora.dividir(4.5, 3)).toEqual(1.5);
 	});
 
 	it('deve retornar "Erro" para divisão por 0', function() {
@@ -23,7 +23,7 @@ describe("Suite de testes de divisão", function() {
 	});
 
 	it('deve retornar 0 para valor 2 inválido', function() {
-		expect(Calculadora.dividir(5, undefined).toEqual(0);
+		expect(Calculadora.dividir(5, undefined)).toEqual(0);
 	});
 
 });
